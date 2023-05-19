5c1757fb09696899405423bcc0cf2ae3b00ed9c7
KEILA ELIZABETH VILLANUEVA PEREZ <KVILLANUEVAPE@uanl.edu.mx>
2023-05-18 21:07:39 -0600

    CAMBIOS 06:07

--- INTERFAZ_CENTRO_MEDICO/Form_Doctor.Designer.cs
+++ INTERFAZ_CENTRO_MEDICO/Form_Doctor.Designer.cs
@@ -35,7 +35,6 @@
             this.label7 = new System.Windows.Forms.Label();
             this.txtEdad = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.btn_RegMenu = new System.Windows.Forms.Button();
             this.btn_AgregarDoc = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -54,19 +53,23 @@
             this.sexo_doc = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.isAcrtivo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.area_medica = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_RegMenu = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGrid_Doctores)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel2
             // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.btn_RegMenu);
             this.panel2.Controls.Add(this.cb_sexo);
             this.panel2.Controls.Add(this.link_NuevaArea);
             this.panel2.Controls.Add(this.cb_AreasM);
             this.panel2.Controls.Add(this.label7);
             this.panel2.Controls.Add(this.txtEdad);
             this.panel2.Controls.Add(this.label6);
-            this.panel2.Controls.Add(this.btn_RegMenu);
             this.panel2.Controls.Add(this.btn_AgregarDoc);
             this.panel2.Controls.Add(this.label1);
             this.panel2.Controls.Add(this.label5);
@@ -84,13 +87,14 @@
             // 
             // cb_sexo
             // 
+            this.cb_sexo.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cb_sexo.FormattingEnabled = true;
             this.cb_sexo.Items.AddRange(new object[] {
             "F",
             "M"});
             this.cb_sexo.Location = new System.Drawing.Point(36, 440);
             this.cb_sexo.Name = "cb_sexo";
-            this.cb_sexo.Size = new System.Drawing.Size(200, 21);
+            this.cb_sexo.Size = new System.Drawing.Size(200, 25);
             this.cb_sexo.TabIndex = 15;
             // 
             // link_NuevaArea
@@ -107,58 +111,55 @@
             // 
             // cb_AreasM
             // 
+            this.cb_AreasM.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cb_AreasM.FormattingEnabled = true;
             this.cb_AreasM.Location = new System.Drawing.Point(36, 505);
             this.cb_AreasM.Name = "cb_AreasM";
-            this.cb_AreasM.Size = new System.Drawing.Size(200, 21);
+            this.cb_AreasM.Size = new System.Drawing.Size(200, 25);
             this.cb_AreasM.TabIndex = 13;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(31, 477);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(147, 25);
+            this.label7.Size = new System.Drawing.Size(149, 23);
             this.label7.TabIndex = 12;
             this.label7.Text = "Area del Doctor";
             // 
             // txtEdad
             // 
+            this.txtEdad.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtEdad.Location = new System.Drawing.Point(36, 369);
             this.txtEdad.Name = "txtEdad";
-            this.txtEdad.Size = new System.Drawing.Size(200, 20);
+            this.txtEdad.Size = new System.Drawing.Size(200, 25);
             this.txtEdad.TabIndex = 11;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(31, 341);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(58, 25);
+            this.label6.Size = new System.Drawing.Size(56, 23);
             this.label6.TabIndex = 10;
             this.label6.Text = "Edad";
             // 
-            // btn_RegMenu
-            // 
-            this.btn_RegMenu.Location = new System.Drawing.Point(36, 25);
-            this.btn_RegMenu.Name = "btn_RegMenu";
-            this.btn_RegMenu.Size = new System.Drawing.Size(75, 23);
-            this.btn_RegMenu.TabIndex = 1;
-            this.btn_RegMenu.Text = "< Regresar";
-            this.btn_RegMenu.UseVisualStyleBackColor = true;
-            this.btn_RegMenu.Click += new System.EventHandler(this.btn_RegMenu_Click);
-            // 
             // btn_AgregarDoc
             // 
+            this.btn_AgregarDoc.BackColor = System.Drawing.Color.Silver;
+            this.btn_AgregarDoc.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_AgregarDoc.FlatAppearance.BorderSize = 0;
+            this.btn_AgregarDoc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_AgregarDoc.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_AgregarDoc.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.btn_AgregarDoc.Location = new System.Drawing.Point(36, 606);
             this.btn_AgregarDoc.Name = "btn_AgregarDoc";
-            this.btn_AgregarDoc.Size = new System.Drawing.Size(200, 23);
+            this.btn_AgregarDoc.Size = new System.Drawing.Size(200, 37);
             this.btn_AgregarDoc.TabIndex = 9;
             this.btn_AgregarDoc.Text = "Agregar Doctor";
-            this.btn_AgregarDoc.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btn_AgregarDoc.UseVisualStyleBackColor = true;
+            this.btn_AgregarDoc.UseVisualStyleBackColor = false;
             this.btn_AgregarDoc.Click += new System.EventHandler(this.btn_AgregarDoc_Click);
             // 
             // label1
@@ -174,61 +175,64 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(31, 412);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(58, 25);
+            this.label5.Size = new System.Drawing.Size(54, 23);
             this.label5.TabIndex = 7;
             this.label5.Text = "Sexo";
             // 
             // txtAmaterno
             // 
+            this.txtAmaterno.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtAmaterno.Location = new System.Drawing.Point(36, 301);
             this.txtAmaterno.Name = "txtAmaterno";
-            this.txtAmaterno.Size = new System.Drawing.Size(200, 20);
+            this.txtAmaterno.Size = new System.Drawing.Size(200, 25);
             this.txtAmaterno.TabIndex = 6;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(31, 273);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(159, 25);
+            this.label4.Size = new System.Drawing.Size(158, 23);
             this.label4.TabIndex = 5;
             this.label4.Text = "Apellido Materno";
             // 
             // txtApaterno
             // 
+            this.txtApaterno.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtApaterno.Location = new System.Drawing.Point(36, 233);
             this.txtApaterno.Name = "txtApaterno";
-            this.txtApaterno.Size = new System.Drawing.Size(200, 20);
+            this.txtApaterno.Size = new System.Drawing.Size(200, 25);
             this.txtApaterno.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(31, 205);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(155, 25);
+            this.label3.Size = new System.Drawing.Size(154, 23);
             this.label3.TabIndex = 3;
             this.label3.Text = "Apellido Paterno";
             // 
             // txtNombre
             // 
+            this.txtNombre.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtNombre.Location = new System.Drawing.Point(36, 168);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(200, 20);
+            this.txtNombre.Size = new System.Drawing.Size(200, 25);
             this.txtNombre.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(31, 140);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 25);
+            this.label2.Size = new System.Drawing.Size(79, 23);
             this.label2.TabIndex = 1;
             this.label2.Text = "Nombre";
             // 
@@ -289,11 +293,39 @@
             this.area_medica.HeaderText = "AREA MEDICA";
             this.area_medica.Name = "area_medica";
             // 
+            // btn_RegMenu
+            // 
+            this.btn_RegMenu.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_RegMenu.BackgroundImage = global::INTERFAZ_CENTRO_MEDICO.Properties.Resources.anterior1;
+            this.btn_RegMenu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_RegMenu.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_RegMenu.FlatAppearance.BorderSize = 0;
+            this.btn_RegMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_RegMenu.Location = new System.Drawing.Point(36, 12);
+            this.btn_RegMenu.Name = "btn_RegMenu";
+            this.btn_RegMenu.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btn_RegMenu.Size = new System.Drawing.Size(51, 51);
+            this.btn_RegMenu.TabIndex = 1;
+            this.btn_RegMenu.UseVisualStyleBackColor = false;
+            this.btn_RegMenu.Click += new System.EventHandler(this.btn_RegMenu_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::INTERFAZ_CENTRO_MEDICO.Properties.Resources.titulo;
+            this.pictureBox1.Location = new System.Drawing.Point(328, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(241, 117);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form_Doctor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
             this.ClientSize = new System.Drawing.Size(1005, 655);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.dataGrid_Doctores);
             this.Controls.Add(this.panel2);
             this.Name = "Form_Doctor";
@@ -301,6 +333,7 @@
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGrid_Doctores)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -333,5 +366,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn sexo_doc;
         private System.Windows.Forms.DataGridViewTextBoxColumn isAcrtivo;
         private System.Windows.Forms.DataGridViewTextBoxColumn area_medica;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
