bcc2bda533703aafb20306abc7ce1195f3b99c9c
jPUENTE23 <70825643+jPUENTE23@users.noreply.github.com>
2023-05-18 23:15:02 -0600

    Revert "Keila"

--- INTERFAZ_CENTRO_MEDICO/Properties/Resources.Designer.cs
+++ INTERFAZ_CENTRO_MEDICO/Properties/Resources.Designer.cs
@@ -249,15 +249,5 @@ namespace INTERFAZ_CENTRO_MEDICO.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap titulo2 {
-            get {
-                object obj = ResourceManager.GetObject("titulo2", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
